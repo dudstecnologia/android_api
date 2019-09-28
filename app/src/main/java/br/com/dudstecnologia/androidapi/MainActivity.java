04aba16957244412bb215c34e032049a3fc5dce0
Eduardo <eduardo@dudstecnologia.com.br>
2019-09-28 10:02:08 -0400

    Layout Finalizado

--- app/src/main/java/br/com/dudstecnologia/androidapi/MainActivity.java
+++ app/src/main/java/br/com/dudstecnologia/androidapi/MainActivity.java
@@ -2,13 +2,29 @@ package br.com.dudstecnologia.androidapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtCriar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtCriar = (TextView) findViewById(R.id.txtCriar);
+
+        txtCriar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent abreCadastro = new Intent(MainActivity.this, ActCadastro.class);
+                startActivity(abreCadastro);
+            }
+        });
     }
 }
